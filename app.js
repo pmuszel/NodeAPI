1d790fd214db890b3f6edcf6cd5b6864f615469c
Piotr Muszel <pmuszel@gmail.com>
2020-12-07 20:02:10 +0100

    Added Posts

--- app.js
+++ app.js
@@ -1,20 +1,87 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
+const { stat } = require('fs');
+
+MONGODB_URI =
+  'mongodb+srv://devadmin:devadmin@nodeserver.sydem.mongodb.net/messages'; // ?retryWrites=true&w=majority'
 
 const app = express();
 
+// const fileStorage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, './images');
+//   },
+//   filename: (req, file, callback) => {
+//     const firstPart = new Date().toISOString().replace(/:/g, '-');
+//     console.log(firstPart);
+//     callback(null, firstPart + '_' + file.originalname);
+//   },
+// });
+
+
+const fileStorage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {
+        cb(null, uuidv4() + path.extname(file.originalname));
+    }
+});
+
+const fileFilter = (req, file, callback) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    callback(null, true);
+  } else {
+    callback(null, false);
+  }
+};
+
+
 app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 app.use('/feed', feedRoutes);
 
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
 
-app.listen(8080);
+  res.status(status).json({
+    message: message,
+  });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    console.log('CONNECTED!');
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,20 +1,133 @@
+const fs = require('fs');
+const path = require('path');
+
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{title: 'First Post', content: 'This is a first post!'}]
-    });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({ message: 'Fetched posts', posts: posts });
+    })
+    .catch(err => catchCode(err, next));
+};
+
+exports.postPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  console.log(req.body);
+  console.log(req.file);
+
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation error, data is incorrect!');
+    error.statusCode = 422;
+    throw error;
+  }
+  if(!req.file) {
+    const error = new Error('No image!');
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path.replace("\\" ,"/");
+  const title = req.body.title;
+  const content = req.body.content;
+
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: imageUrl,
+    creator: { name: 'Piotr' },
+  });
+
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: post,
+      });
+    })
+    .catch(err => catchCode(err, next));
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Could not find post');
+        error.statusCode = 404;
+        throw error;
+      }
+
+      res.status(200).json({ message: 'Post fetched', post: post });
+    })
+    .catch(err => catchCode(err, next));
+};
+
+const catchCode = (err, next) => {
+  if (!err.statusCode) {
+    err.statusCode = 500;
+  }
+  next(err);
 };
 
 
-exports.postPost = (req, res, next) => {
+exports.updatePost = (req, res, next) => {
+    const postId = req.params.postId;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation error, data is incorrect!');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    let imageUrl = req.body.image;
     const title = req.body.title;
     const content = req.body.content;
 
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: {
-            id: new Date().toISOString(),
-            title: title,
-            content: content
+    if(req.file) {
+        imageUrl = req.file.path.replace("\\" ,"/");
+    }
+
+    if(!imageUrl) {
+        const error = new Error('No file picked!');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    Post.findById(postId)
+    .then(post => {
+        if(!post) {
+            const error = new Error('Could not find post');
+            error.statusCode = 404;
+            throw error;
         }
-    });
+
+        if(imageUrl !== post.imageUrl) {
+            clearImage(post.imageUrl);
+        }
+
+        post.title = title;
+        post.imageUrl = imageUrl;
+        post.content = content;
+
+        return post.save();
+    })
+    .then(result => {
+        res.status(200).json({message: 'Post updated!', post: result})
+    })
+    .catch(err => catchCode(err, next));
+
+};
+
+
+
+const clearImage = filePath => {
+    const file = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
 };
